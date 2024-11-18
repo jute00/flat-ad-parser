6c4639f4883d389b49c38680af8ee6f44d48fa0d
jute <uspeca@gmail.com>
2024-11-18 15:27:27 +0300

    fix o

--- app/Services/OParser.php
+++ app/Services/OParser.php
@@ -11,7 +11,7 @@ class OParser extends AbstractParser
 
     protected function getDocumentUrl(): string
     {
-        return 'https://r.onliner.by/sdapi/ak.api/search/apartments?price[min]=200&price[max]=400&currency=usd&only_owner=true&bounds[lb][lat]=53.84462779782495&bounds[lb][long]=27.381706237792972&bounds[rt][lat]=53.96578102161269&bounds[rt][long]=27.70339965820313&order=created_at:desc&page=1&v=0.6306061828305893';
+        return 'https://r.onliner.by/ak/?price%5Bmin%5D=250&price%5Bmax%5D=400&currency=usd&metro%5B%5D=red_line&metro%5B%5D=blue_line&metro%5B%5D=green_line&only_owner=true#bounds%5Blb%5D%5Blat%5D=53.79821757312943&bounds%5Blb%5D%5Blong%5D=27.365226745605472&bounds%5Brt%5D%5Blat%5D=53.99949568180132&bounds%5Brt%5D%5Blong%5D=27.68692016601563&order=created_at:desc';
     }
 
     protected function getXpathQueryExpression(): string
